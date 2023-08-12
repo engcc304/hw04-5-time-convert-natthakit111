c39215028bb3cde7b27bc2cfc7763f2742f3c7bc
natthakit111 <natthakit_ya66@live.ac.th>
2023-08-12 19:15:35 +0700

    update hw4-4

--- Homework/HW04/hw04-4-triangle.cpp
+++ Homework/HW04/hw04-4-triangle.cpp
@@ -13,3 +13,15 @@
     Output:
         Triagle Area : 7.5
 */
+#include <stdio.h>
+
+int main() {
+    float height, base;
+    printf("Enter H & W :\n");
+    scanf("%f %f", &height, &base);
+
+    float area = (height * base) / 2;
+    printf("Triangle Area : %.1f\n", area);
+
+    return 0;
+}//end function
